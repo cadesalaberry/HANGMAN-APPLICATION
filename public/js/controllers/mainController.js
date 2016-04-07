4f2129fcffd8dd65260a815c2e1bb91e29f59535
lauraneblettery <laurane.blettery@gmail.com>
2016-04-07 23:42:42 +0200

    add files

--- public/js/controllers/mainController.js
+++ public/js/controllers/mainController.js
@@ -1,20 +1,29 @@
 // MAIN CONTROLLER
 function mainController($scope, $http, PenduService) {
 	$scope.Pendu = "PENDU";
-	$scope.failedGuess = [];
-	$scope.count = 0;
 	$scope.chercherLettre = function () {
+		$scope.count = 0;
 		for (var i = 1; i < $scope.garder.length - 1; i++) {
 			if ($scope.lettre == $scope.garder[i]) {
-				$scope.count++
+				$scope.count++;
+				$scope.motCache = $scope.motCache.split("").map(function (lettres, index) {
+					if (index == i * 2) return $scope.lettre;
+					return lettres;
+				}).join('');
 			}
 		}
 		if ($scope.count == 0) {
 			$scope.failedGuess.push($scope.lettre);
 		}
+		
+		$scope.lettre = "";
+		if($scope.garder == $scope.motCache.replace(/[ ]+/g, ""))
+			$scope.felicitation = "Félicitation Wilder, tu as trouvé le bon mot !!!";
 	}
 
 	$scope.pendu = function () {
+		$scope.failedGuess = [];
+		$scope.count = 0;
 		$scope.garder = $scope.mot;
 		$scope.tiret = "";
 		for (var lettre = 0; lettre < $scope.mot.length - 2; lettre++) {
@@ -24,9 +33,4 @@ function mainController($scope, $http, PenduService) {
 		$scope.mot = "";
 	}
 
-	function load() {
-		PenduService.get().then(function (res) {
-			$scope.pendu = res.data;
-		});
-	}
 }
