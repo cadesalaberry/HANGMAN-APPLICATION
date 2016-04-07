2f81661b14b2d589ebb08cccd234f2d39f6fa346
lauraneblettery <laurane.blettery@gmail.com>
2016-04-07 22:14:18 +0200

    add files

--- public/js/controllers/mainController.js
+++ public/js/controllers/mainController.js
@@ -1,24 +1,32 @@
 // MAIN CONTROLLER
 function mainController($scope, $http, PenduService) {
 	$scope.Pendu = "PENDU";
-
+	$scope.failedGuess = [];
+	$scope.count = 0 ;
 	$scope.chercherLettre = function () {
-		$scope.failedGuess = [];
-		$scope.failedGuess.push($scope.lettre);
-	}
-		$scope.pendu = function () {
-			$scope.garder = $scope.mot;
-			$scope.tiret = "";
-			for (var lettre = 0; lettre < $scope.mot.length - 2; lettre++) {
-				$scope.tiret += " _";
+		for (var i=1; i < $scope.garder.length-1; i++){
+			if ($scope.lettre == $scope.garder[i]){
+				$scope.count++
 			}
-			$scope.motCache = $scope.mot[0] + $scope.tiret + $scope.mot[$scope.mot.length - 1];
-			$scope.mot = "";
 		}
-
-		function load() {
-			PenduService.get().then(function (res) {
-				$scope.pendu = res.data;
-			});
+		if ($scope.count == 0){
+			$scope.failedGuess.push($scope.lettre);
 		}
 	}
+
+	$scope.pendu = function () {
+		$scope.garder = $scope.mot;
+		$scope.tiret = "";
+		for (var lettre = 0; lettre < $scope.mot.length - 2; lettre++) {
+			$scope.tiret += " _";
+		}
+		$scope.motCache = $scope.mot[0] + $scope.tiret + $scope.mot[$scope.mot.length - 1];
+		$scope.mot = "";
+	}
+
+	function load() {
+		PenduService.get().then(function (res) {
+			$scope.pendu = res.data;
+		});
+	}
+}
